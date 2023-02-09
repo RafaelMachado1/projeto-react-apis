3f01db062a53f4e368cc294fee08214758dacb70
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-02-09 19:12:08 -0300

    coordinator

--- pokedex/src/context/GlobalState.js
+++ pokedex/src/context/GlobalState.js
@@ -2,12 +2,81 @@ import { GlobalContext } from "./GlobalContext";
 
 
 const GlobalState = () => {
+    const [pokemons, setPokemons] = useState([])
+    const [pokedex, setPokedex] = useState([])
+    const [activeModal, setActiveModal] = useState(1)
+  
 
+    useEffect(() => {
+      getPokemons()
+    }, [])
+  
+    const getPokemons = async () => {
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+        setPokemons(response.data.results)
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
-const context = {
+    useEffect(() => {
+        if (pokedex.length > 0) {
+          const pokedexString = JSON.stringify(pokedex)
+          localStorage.setItem('pokedex', pokedexString)
+        }
+      }, [pokedex])
     
-}
+      useEffect(() => {
+        const getSavePokedex = JSON.parse(localStorage.getItem('pokedex'))
+        if (getSavePokedex !== null) {
+          setPokedex(getSavePokedex)
+        }
+      }, [])
+    
+      const addToPokedex = (pokemonToAdd) => {
+        const isOnPokedex = pokedex.find((pokemonInPokedex) => pokemonInPokedex.name === pokemonToAdd.name);
+        if (!isOnPokedex) {
+          const newPokedex = [...pokedex, pokemonToAdd]
+          setPokedex(newPokedex)
+        }
+      }
+    
+      const removePokedex = (pokemonToRemove) => {
+        const newPokedex = pokedex.filter((pokemonInPokedex) => pokemonInPokedex.name !== pokemonToRemove.name)
+        window.localStorage.removeItem('pokedex')
+        setPokedex(newPokedex)
+      }
 
+      const renderPokemonList = pokemons.map((pokemon) => {
+        return <PokemonCard
+          key={pokemon.url}
+          pokemonName={pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}
+          pokemon={pokemon.name}
+          addToPokedex={addToPokedex} />
+      })
+    
+      const renderPokedex = pokedex.map((pokemon) => {
+        return <PokemonCard
+          key={pokemon.name}
+          pokemonName={pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}
+          pokemon={pokemon.name}
+          removePokedex={removePokedex}
+        />
+      })
+
+    const context = {
+        pokemons,
+        setPokemons,
+        pokedex,
+        setPokedex,
+        addToPokedex,
+        removePokedex,
+        renderPokemonList,
+        renderPokedex,
+        activeModal,
+        setActiveModal
+      }
 
     return(
         <GlobalContext.Provider value={context} >
